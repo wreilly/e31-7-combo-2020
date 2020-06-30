992047e203f5d3e0c62837e0c049695ea5e92edb
William Reilly <wreilly2001@gmail.com>
2020-06-30 10:01:23 -0400

    COMPLETE App Shell: MatSidenav close()

--- client/src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ client/src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-list',
@@ -7,11 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavListComponent implements OnInit {
 
+  @Output('myToggleMatSidenavEventEmitterSidenavName')
+  myToggleMatSidenavEventEmitterSidenav: EventEmitter<any> = new EventEmitter<any>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-
+  myCloseSidenav() {
+    // Here in Sidenav, we only Close, not Toggle, really ...
+    this.myToggleMatSidenavEventEmitterSidenav.emit(null); // null's all we need
+  }
 
 }
