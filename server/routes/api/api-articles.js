2a3a647f836c9e2e7343f359c06a889e5594b840
William Reilly <wreilly2001@gmail.com>
2020-07-07 17:26:10 -0400

    Multer in Server router; Client HTTP Service for Articles

--- server/routes/api/api-articles.js
+++ server/routes/api/api-articles.js
@@ -1,6 +1,51 @@
 const express = require('express');
 const apiArticlesRouter = express.Router();
 
+/* ######################################################### */
+/* ###############  MULTER HERE IN ROUTER, SIMPLY ########## */
+/* ######################################################### */
+// See extensive notes in 2018 server ...
+
+const myMulter = require('multer');
+const myMkdirp = require('mkdirp');
+
+const myDiskStorage = myMulter.diskStorage({
+    destination: myDestinationFunction,
+    filename: myFilenameFunction
+});
+
+const myPhotosUploadMulter = myMulter({
+    storage: myDiskStorage,
+    fileFilter: myFileFilterFunction
+});
+
+function myDestinationFunction(req, file, callback) {
+    const destination = 'public/img';
+
+    myMkdirp(destination, function(err) {
+        if(err) {
+            console.log('Dang. myMkdirp failed. ', err);
+        } else {
+            console.log('Great. myMkdirp destination is: ', destination);
+            callback(null, destination);
+        }
+    })
+}
+
+function myFilenameFunction(req, file, callback) {
+    console.log('Router. Multer. myFilenameFunction file.originalname: ', file.originalname);
+    callback(null, `sometimes__${Date.now()}_${file.originalname}`);
+    // callback(null, 'sometimes__' + Date.now() + '_' + file.originalname);
+}
+
+function myFileFilterFunction(req, file, callback) {
+    callback(null, true); // TODO filter on .JPG, .PNG etc. T.B.D. (sigh)
+}
+
+/* ######################################################### */
+/* ###############  /MULTER HERE IN ROUTER, SIMPLY ########## */
+/* ######################################################### */
+
 const apiArticleControllerHereInApi = require('.././../controllers/api/api-articleController');
 
 /* ***********  TOC  ********
@@ -34,12 +79,9 @@ apiArticlesRouter.use(
     }
 );
 
-
-apiArticlesRouter.get('/',
-    function(req, res, next) {
-    apiArticleControllerHereInApi.apiGetAllArticles(req, res, next)
-})
-
+/* ************************************************** */
+/* ******** GET '/api/v1/articles/12345' ************ */
+/* ************************************************** */
 apiArticlesRouter.get('/:idHere',
     function (req, res, next) {
         console.log('API Router - getArticleById - idHere is: ', req.params.idHere);
@@ -57,7 +99,35 @@ apiArticlesRouter.get('/:idHere',
     }
 ) // /GET '/:id'
 
-apiArticlesRouter.post('/',
+
+/* ************************************************** */
+/* ******** GET '/api/v1/articles/' ************ */
+/* ************************************************** */
+apiArticlesRouter.get('/',
+    function(req, res, next) {
+    apiArticleControllerHereInApi.apiGetAllArticles(req, res, next)
+})
+
+
+
+/* ************************************************** */
+/* ******** POST '/api/v1/articles/'  ************ */
+/* ************************************************** */
+apiArticlesRouter.post(
+    '/',
+
+    /*
+    TIME FOR MULTER HERE IN 2020!
+    Below line just handles text form field data.
+    No handling of FILES; see elsewhere for that.
+
+    William, do NOT forget to "invoke()" the
+    damned thing.
+     */
+    // myPhotosUploadMulter.array, // << !! WRONG !!
+    myPhotosUploadMulter.array(),
+
+
     function (req, res, next) {
 
         console.log('REST API ROUTER POST / req.body: ', req.body);
